6c15240e95310176108ea87fe5d39db77ba2ddbd
khinnadiko <khin.nadi1@mail.kmutt.ac.th>
2025-04-06 15:51:39 +0700

    Add HW#5 files

---
+++ todo-api/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { logger } from 'hono/logger';
+import todoRoutes from './routes/todo.routes.ts';
+
+const app = new Hono();
+
+app.use('*', logger());
+
+app.get('/', (c) => {
+	return c.json({
+		message: 'Welcome to our Todo API!',
+		status: 'Server is running',
+	});
+});
+
+app.route('/todos', todoRoutes);
+
+const port = 3000;
+
+serve(
+	{
+		fetch: app.fetch,
+		port,
+	},
+	(info) => console.log(`Server running at http://localhost:${info.port}`)
+);
---
+++ todo-api/src/routes/todo.routes.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import * as TodoController from '../controllers/todo.controller.ts';
+import { validateCreateTodo, validateIdParam, validatePatchTodo, validateUpdateTodo } from "../middlewares/validators.ts";
+
+const todoRoutes = new Hono();
+
+todoRoutes.get('/', TodoController.getTodos);
+todoRoutes.get('/search', TodoController.searchTodos);
+todoRoutes.get('/:id', validateIdParam, TodoController.getTodo);
+todoRoutes.post('/', validateCreateTodo, TodoController.createTodo);
+todoRoutes.put('/:id', validateIdParam, validateUpdateTodo, TodoController.updateTodo);
+todoRoutes.patch('/:id', validateIdParam, validatePatchTodo, TodoController.patchTodo);
+todoRoutes.delete('/:id', validateIdParam, TodoController.deleteTodo);
+
+export default todoRoutes;
